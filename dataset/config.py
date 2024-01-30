bd4419a6f26ec237353de1a3c8241309f5584e8a
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2024-01-30 15:44:24 +0530

    dataset collection

---
+++ dataset/config.py
@@ -0,0 +1,5 @@
+URLS = [
+    "https://www.youtube.com/watch?v=ORrrKXGx2SE",
+    "https://www.youtube.com/watch?v=hTUyzF4v9KA",
+    "https://www.youtube.com/watch?v=_SOSBFmH_Gs"
+]
---
+++ dataset/main.py
@@ -0,0 +1,25 @@
+import os
+import cv2
+from pytube import YouTube
+from config import URLS
+
+def download_video(url, filename):
+    yt = YouTube(url)
+    yt.streams.first().download(filename=filename)
+
+def save_frames(filename, output_folder):
+    vidcap = cv2.VideoCapture(filename)
+    success, image = vidcap.read()
+    count = 0
+    while success:
+        cv2.imwrite(os.path.join(output_folder, f"frame{count}.jpg"), image)
+        success, image = vidcap.read()
+        count += 1
+
+output_folder = "frames"
+os.makedirs(output_folder, exist_ok=True)
+for i, url in enumerate(URLS):
+    filename = f"video{i}.mp4"
+    download_video(url, filename)
+    save_frames(filename, output_folder)
+    os.remove(filename)
