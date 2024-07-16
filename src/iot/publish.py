b2c1b2c93ab4d9d3f68ea8e067a415e2b939281d
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2024-07-16 20:31:29 +0530

    commented out hardcoded device id

--- src/iot/publish.py
+++ src/iot/publish.py
@@ -61,6 +61,7 @@ if __name__ == '__main__':
     mqtt_client = MqttClient(broker, port, topic, client_id)
 
     while True:
+        # mqtt_client.send_data("0")
         mqtt_client.send_data(os.getenv("DEVICE"))
         print(f"Published payload")
         time.sleep(5)  # Send data every 5 seconds
